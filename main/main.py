9878c8126884dfbef2bda6cd1b04ca50d480ccba
maqsadjon2024 <maqsadjonqurbondurdiev58@gmail.com>
2025-11-29 17:11:44 +0500

    Create main.py

---
+++ main/main.py
@@ -0,0 +1,28 @@
+# 1-misol
+uzunlik = lambda s: len(s)
+
+print(uzunlik("hello"))
+
+# 2-misol
+tekshir = lambda x: "musbat" if x > 0 else ("manfiy" if x < 0 else "nol")
+
+print(tekshir(5))
+print(tekshir(-3))
+print(tekshir(0))
+
+# 3-misol
+res = lambda a, b: (a * b) / 2
+
+print(12)
+
+# 4-misol
+juftmi = lambda x: x % 2 == 0
+
+print(juftmi(4))
+print(juftmi(7))
+
+# 5-misol
+katta_son = lambda x: x > 10
+
+print(katta_son(15))  
+print(katta_son(8))   
